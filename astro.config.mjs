3585a8bfff528c162777f5d2d9ab1ce8374d7a30
Destiny Simmons <34193988+kismet-lee@users.noreply.github.com>
2024-03-23 18:42:26 +0900

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:4321'
+      : 'https://owddm-workshop.netlify.app/',
+  integrations: [tailwind(), sitemap(), icon()],
+});
